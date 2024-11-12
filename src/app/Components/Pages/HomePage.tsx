33cdb3a978c58eac078f32842b63a1f82e050573
Karim-Zaf <kzaafrani@gmail.com>
2024-11-12 22:11:16 +0100

    adding my identity picutre

--- src/app/Components/Pages/HomePage.tsx
+++ src/app/Components/Pages/HomePage.tsx
@@ -1,13 +1,19 @@
+
+import { Image } from 'lucide-react'
 import React from 'react'
+
 
 function HomePage() {
   return (
-    <div>
-            <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
+    <>
+      <div className='flex justify-center items-center gap-10 flex-col'>
+            <img src='identity_picture.jpeg' className='z-0 rounded-[400px] w-[250px]'/>
+              <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
 
-                <span>Karim Zaafrani</span>
-            </h2>
-    </div>
+                  <span>Karim Zaafrani</span>
+              </h2>
+      </div>
+    </>
   )
 }
 

971f18f647372a9a689f69529d11a4ede2c60bfc
Karim-Zaf <kzaafrani@gmail.com>
2024-11-12 21:54:29 +0100

    adds

---
+++ src/app/Components/MainPage.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { useState } from 'react'
+import HomePage from './Pages/HomePage';
+
+function MainPage() {
+
+    const [page , setPage] = useState("home") ;
+
+  return (
+    <>
+      
+          
+    
+               
+    </>
+  )
+}
+
+export default MainPage

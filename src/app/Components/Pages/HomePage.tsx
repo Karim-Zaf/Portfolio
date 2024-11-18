7af5505e9f848bfe0402f885bf457eb6e436ae1a
Karim-Zaf <kzaafrani@gmail.com>
2024-11-18 19:50:11 +0100

    cursor animation

--- src/app/Components/Pages/HomePage.tsx
+++ src/app/Components/Pages/HomePage.tsx
@@ -1,23 +1,34 @@
-
-import { Image } from 'lucide-react'
-import React from 'react'
-
+import React from 'react';
+import { Typewriter } from "react-simple-typewriter";
 
 function HomePage() {
   return (
     <>
-      <div className='flex justify-center items-center gap-10 flex-col'>
-            <img
+      <div className='flex justify-center items-center gap-10 flex-col top-6'>
+        
+       
+        <img
           src='identity_picture.jpeg'
-          className='z-0 rounded-[400px] w-[250px] border-animated'
+          className='z-0 rounded-full w-[250px] border-animated'
         />
-              <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
-
-                  <span>Karim Zaafrani</span>
-              </h2>
+        <div className="relative flex flex-col justifys-center items-center md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white  gap-2 md:gap-8">
+            <span>Karim Zaafrani</span>
+            
+        </div>
       </div>
+      <div className="text-3xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white">
+          <Typewriter
+          words={[
+            'Full Stack Developer',
+            'Software Engineer',
+            'Competitive Programmer',
+          ]}
+          loop
+          
+        /></div>
+          
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;

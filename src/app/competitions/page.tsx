b498ec45e4314269d3b9adb6a98d73d138dfae25
Karim-Zaf <kzaafrani@gmail.com>
2025-01-17 10:45:20 +0100

    fixing the dependecies

--- src/app/competitions/page.tsx
+++ src/app/competitions/page.tsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import { Pagination, Navigation } from 'swiper';
+import { Pagination, Navigation } from 'swiper/modules';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
 
 export default function CompetitionsPage() {

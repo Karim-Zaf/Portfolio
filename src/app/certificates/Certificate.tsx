e7199ceac846b8d2543e9a7009b7a56bcfa57e78
Karim-Zaf <kzaafrani@gmail.com>
2025-02-11 00:23:07 +0100

    fixing my certificate page

--- src/app/certificates/Certificate.tsx
+++ src/app/certificates/Certificate.tsx
@@ -1,37 +1,45 @@
 "use client";
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@radix-ui/react-accordion';
-import { Link } from 'lucide-react';
+import { ChevronDown, Link } from 'lucide-react';
 import { useState } from 'react';
 
-export default function Certificate({ title, skills, ImageLink, Provider, WebSiteLink }: any) {
+export default function Certificate({ title, skills, ImageLink, Provider, WebSiteLink ,index}: any) {
   let [isImage,setIsImage] = useState(0)
 
 
   return (
+    <Accordion type="single" collapsible>
+  <AccordionItem value="item-1">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
+
     
-    <AccordionItem className='md:w-[700px] w-[90%] m-auto pt-0 bg-neutral-800  mb-4  rounded-xl border-neutral-400 border-2 ' value="item-1">
-        <AccordionTrigger className='w-full flex  h-full rounded-t-xl p-2'>
+    // <AccordionItem className='md:w-[700px] w-[90%] m-auto pt-0 bg-neutral-800  mb-4  rounded-xl border-neutral-400 border-2 ' value={`item-${index}`}>
+    //     <AccordionTrigger className='w-full flex  h-full rounded-t-xl p-2'>
            
-            <img
-              src={`Certificates/Providers/${Provider.toLowerCase()}.svg`}
-              alt={`${Provider} logo`}
-              className='w-12 h-12 bg-slate-200  rounded-full m-1 '
-            />
-            <div className='w-full m-0 p-0 ml-5 mr-3 flex flex-col justify-center'>
-              <h3 className='text-lg font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white'>
-                    {title}
-                </h3>
-                <p className='text-sm text-white'>{skills.join(', ')}</p>
-            </div>
+    //         <img
+    //           src={`Certificates/Providers/${Provider.toLowerCase()}.svg`}
+    //           alt={`${Provider} logo`}
+    //           className='w-12 h-12 bg-slate-200  rounded-full m-1 '
+    //         />
+    //         <div className='w-full m-0 p-0 text-left pl-8'>
+    //             <h3 className='text-lg font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white'>
+    //                 {title}
+    //             </h3>
+    //             <p className='text-sm text-white'>{skills.join(', ')}</p>
+    //         </div>
 
-          
-        </AccordionTrigger>
+    //     </AccordionTrigger>
 
-        <AccordionContent className='w-full bg-neutral-700 rounded-b-xl items-center justfiy-center py-3'>
-          <img src={`Certificates/${ImageLink}`} alt={title} className='m-auto md:w-96 w-[90%] h-auto  rounded' />
-        </AccordionContent>
-      </AccordionItem>
+    //     <AccordionContent className='w-full bg-neutral-700 rounded-b-xl items-center justfiy-center py-3'>
+    //       <img src={`Certificates/${ImageLink}`} alt={title} className='m-auto md:w-96 w-[90%] h-auto  rounded' />
+    //     </AccordionContent>
+    //   </AccordionItem>
 
      
     
--- src/app/certificates/page.tsx
+++ src/app/certificates/page.tsx
@@ -3,29 +3,63 @@ import { Card, CardContent } from '@/components/ui/card';
 import React from 'react';
 import certificates from './cerficates.json'
 import Certificate from './Certificate';
-import { Accordion } from '@radix-ui/react-accordion';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+
+
 function Certificates() {
-  
+  let cnt = 0 ;
   return (
     <div className='w-full flex flex-col items-center py-8 min-h-screen md:mt-28 mt-20'>
+
       <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8 border-b-4 pb-2">
         <span>My Certificates</span>
       </h2>
       <div className='mt-8 w-full text-xl text-white h-1 bg-gradient-to-r mb-16 text-center'>
         Here you will find a list of my certificates
       </div>
-      {/* <div className='flex flex-col'>
+      
+      <div className='flex flex-col'>
+        
+        <Accordion type="single" collapsible className="w-full text-white">
+                
         {
-          certificates.map(({ title, skills, ImageLink, Provider ,WebSiteLink }) => {
-            return <>
-              <Accordion type="single" collapsible className="w-full">
-                <Certificate title={title} skills ={skills} ImageLink={ImageLink} Provider = {Provider} WebSiteLink={WebSiteLink} />
-              </Accordion>
-            </>
-        })
-      }
-      </div> */}
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+            certificates.map(({ title, skills, ImageLink, Provider ,WebSiteLink }) => {
+              return (
+              <AccordionItem className='md:w-[700px] w-[90%] m-auto pt-0 bg-neutral-800  mb-4  rounded-xl border-neutral-400 border-2 pr-3' value={`item-${++cnt}`}>
+                <AccordionTrigger className='w-full flex  h-full rounded-t-xl p-2'>
+                  
+                    <img
+                      src={`Certificates/Providers/${Provider.toLowerCase()}.svg`}
+                      alt={`${Provider} logo`}
+                      className='w-12 h-12  bg-slate-200  rounded-full m-1 '
+                    />
+                    <div className='w-full m-0 p-0 text-left pl-8'>
+                        <h3 className='text-lg font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white'>
+                            {title}
+                        </h3>
+                        <p className='text-sm text-white'>{skills.join(', ')}</p>
+                    </div>
+
+                </AccordionTrigger>
+
+                <AccordionContent className='w-full bg-neutral-700 rounded-b-xl items-center justfiy-center py-3'>
+                  <img src={`Certificates/${ImageLink}`} alt={title} className='m-auto md:w-96 w-[90%] h-auto  rounded' />
+                </AccordionContent>
+              </AccordionItem>
+
+              );
+          })
+        }
+
+        </Accordion>
+      </div>
+      {/* <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
         {certificates.map(({ title, skills, ImageLink, Provider,WebSiteLink }) => (
           <Card key={title} className='sm:w-[420px] w-[90%] m-auto shadow-lg bg-neutral-800'>
             <CardContent className='p-6 flex flex-col justify-center items-center gap-7'>
@@ -52,7 +86,7 @@ function Certificates() {
             </CardContent>
           </Card>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
